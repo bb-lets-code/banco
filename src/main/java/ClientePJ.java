a21989939815b1e7183a868ba294b02884f60d17
F6239902 Leticia Ferreira Reis <leticia.reis@bb.com.br>
2022-06-10 15:02:31 -0300

    Criação da classe PJ

--- src/main/java/ClientePJ.java
+++ src/main/java/ClientePJ.java
@@ -1,3 +1,23 @@
 public class ClientePJ extends Client{
-    
+    private String cnpj;
+    private Double transferWithdrawIncomeRate;
+    private Double savingsIncomeRate;
+
+    public ClientePJ(String cnpj, Double transferWithdrawIncomeRate, Double savingsIncomeRate){
+        this.cnpj = cnpj;
+        this.transferWithdrawIncomeRate = transferWithdrawIncomeRate;
+        this.savingsIncomeRate  = savingsIncomeRate;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public Double getTransferWithdrawIncomeRate() {
+        return transferWithdrawIncomeRate;
+    }
+
+    public Double getSavingsIncomeRate() {
+        return savingsIncomeRate;
+    }
 }
