e268fcac809ff9679e8508a9809ee9b75bedb910
F6239902 Leticia Ferreira Reis <leticia.reis@bb.com.br>
2022-06-13 15:22:39 -0300

    Adição de taxas para PJ

--- src/main/java/ClientePJ.java
+++ src/main/java/ClientePJ.java
@@ -1,24 +1,21 @@
 public class ClientePJ extends Client{
     private String cnpj;
-    private Double transferWithdrawIncomeRate;
-    private Double savingsIncomeRate;
+    private final Double transferWithdrawTax = 0.005;
+    private final Double investmentBonus = 0.02;
 
     public ClientePJ(String cnpj, String nomeCliente){
         super(nomeCliente);
         this.cnpj = cnpj;
-        this.transferWithdrawIncomeRate = 0.005;
-        this.savingsIncomeRate  = 0.005;
     }
 
     public String getCnpj() {
         return cnpj;
     }
 
-    public Double getTransferWithdrawIncomeRate() {
-        return transferWithdrawIncomeRate;
+    public Double getInvestmentBonus() {
+        return investmentBonus;
     }
-
-    public Double getSavingsIncomeRate() {
-        return savingsIncomeRate;
+    public Double getTransferWithdrawTax() {
+        return transferWithdrawTax;
     }
 }
