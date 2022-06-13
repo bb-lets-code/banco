a9df0c5012791a90c3be4b09e17c5b68d2d73f94
F4352981-Igor <igor.nascimento@bb.com.br>
2022-06-13 17:30:37 -0300

    Adição do argumento toAccount em Transferência

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -25,13 +25,13 @@ abstract class Account {
     }
     
 
-    public abstract boolean openAccount();
+    // public abstract boolean openAccount();
         
-    public abstract boolean transfer( long account , double value);
+    public abstract boolean transfer( Account toAccount , double value);
     
     public abstract boolean deposit(double value);
 
     public abstract boolean withdraw(double value);
 
-    public abstract boolean endAccount();
+    // public abstract boolean endAccount();
 }
