8102938078819c509b2c77de5206a5c0bcb4a807
F4352981-Igor <igor.nascimento@bb.com.br>
2022-06-13 15:28:15 -0300

    Criação do main para teste

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,18 @@
 public class Application {
     public static void main(String[] args) {
-        
+        ClientePF clientePF = new ClientePF(
+                "João"
+        );
+        ClientePJ clientePJ = new ClientePJ("", "null", 0.005, 0.005);
+        try {
+            SavingsAccount savingsAccount = new SavingsAccount(
+                clientePF,
+                1
+            );
+                    savingsAccount.deposit(100);
+                    System.out.println( savingsAccount.totalBalance());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
