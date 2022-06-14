6eeca001f31ef7138f839e066e309f4c0db4ae03
IGOR RHAMON CARDOSO DO NASCIMENTO <igor-rh@hotmail.com>
2022-06-13 22:54:09 -0300

    Utilizando BigDecimal para operações

--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -7,28 +7,31 @@ public class SavingsAccount extends Account {
 
     public SavingsAccount(Client client, long number) {
         super( number, client);
-        if( client instanceof ClientePJ ){
-            throw new IllegalArgumentException("Cliente PJ não pode ter conta de poupança");
-        }
-
-        this.income = BigDecimal.valueOf(0);
-        
+        if( client instanceof ClientePJ ) 
+            throw new IllegalArgumentException(
+                "Cliente PJ não pode ter conta de poupança"
+            );
+        else this.income = BigDecimal.valueOf(0);        
     }
 
 
     private BigDecimal investment(BigDecimal value) {
-        return value.multiply(interestRate);
+        return value.multiply(interestRate); // calcula juros
     }
 
 
     @Override
-    public boolean deposit(double value) {
-        if(value > 0){
-            BigDecimal amountTemp = BigDecimal.valueOf(getAmount());
-            amountTemp = amountTemp.add(BigDecimal.valueOf(value));
-            setAmount(amountTemp.doubleValue());
-            BigDecimal incomeTemp = BigDecimal.valueOf(value);
-            this.income =  this.income.add(investment(incomeTemp));
+    public boolean deposit(double valueDeposit) {
+        if(valueDeposit > 0){
+            setAmount(
+                BigDecimal.valueOf(getAmount()) //Valor atual do saldo
+                .add(BigDecimal.valueOf(valueDeposit)) //Adicionando o valor do depósito
+                .doubleValue() //Convertendo para double
+            );
+            this.income =  this.income.add( //Adicionando o valor do juros
+                investment(BigDecimal.valueOf(valueDeposit)) //Calculando o valor do rendimento
+            );
+            
             return true;
         }else{
             throw new IllegalArgumentException("Valor inválido");   
@@ -39,11 +42,13 @@ public class SavingsAccount extends Account {
 
     @Override
     public boolean withdraw(double value) {
-        if(getAmount() + this.income.doubleValue() >= value){
-            double amountTemp = getAmount();
-            amountTemp -= value;
-            setAmount(amountTemp);
-            return true;
+        BigDecimal valueDeposit = BigDecimal.valueOf(value); //Valor do saque
+        BigDecimal totalBalance = BigDecimal.valueOf(getAmount()).add(this.income); //Saldo total
+
+        if(totalBalance.compareTo(valueDeposit) >= 0){ //Verifica se o saldo é maior ou igual ao valor do saque
+            
+            setAmount(totalBalance.subtract(valueDeposit).doubleValue()); //Atualizando o saldo
+            return true; 
         }else{
             throw new IllegalArgumentException("Saldo insuficiente");
         }
@@ -51,49 +56,44 @@ public class SavingsAccount extends Account {
     
     @Override
     public boolean transfer( Account toAccount  , double value) {
-        
-        if(toAccount != null){
-            if(getAmount() < value){
+        BigDecimal valueDeposit = BigDecimal.valueOf(value); //Valor do depósito
+        BigDecimal totalBalance = BigDecimal.valueOf(getAmount()).add(this.income); //Saldo total
+        if(toAccount != null){ //Verificando se a conta de destino existe
+            if(totalBalance.compareTo(valueDeposit) <= 0){ //Verificando se o saldo é suficiente
                 throw new IllegalArgumentException("Saldo insuficiente");
             }else{
-                double amountTemp = getAmount();
-                amountTemp -= value;
-                setAmount(amountTemp);
-                toAccount.deposit(value);
+                setAmount(
+                    totalBalance //Valor atual do saldo
+                    .subtract(valueDeposit) //Subtraindo o valor do depósito
+                    .doubleValue() //Convertendo para double
+                );
+                toAccount.deposit(value); //Adicionando o valor do depósito na conta de destino
                 return true;
             }
         }else{
-            throw new IllegalArgumentException("Conta inválida");
+            throw new IllegalArgumentException("Conta inválida"); //Conta de destino não existe
         }
     }
     
     
-    public String calculateIncome(){
-        return "R$ " + this.income.toString();
-    }
+    // public String calculateIncome(){
+    //     return "R$ " + this.income.toString();
+    // }
 
     private String totalBalance(){
-        return "N° da Conta: "+ this.getNumber() +" Saldo: " + getAmount() + " | Rendimento: " + this.income + " | Total: " + (getAmount() + this.income.doubleValue());
+        BigDecimal bigDecimalAmount = BigDecimal.valueOf(getAmount());
+        return "N° da Conta: "+ this.getNumber() +
+                " Saldo: " + getAmount() + 
+                " | Rendimento: " + this.income + 
+                " | Total: " + (bigDecimalAmount.add(this.income).doubleValue());
     }
 
 
 
-    // @Override
-    // public boolean openAccount() {
-    //     // TODO Auto-generated method stub
-    //     return false;
-    // }
-    // @Override
-    // public boolean endAccount() {
-    //     // TODO Auto-generated method stub
-    //     return false;
-    // }
-
-
-    //TODO: criar toString
+    
     @Override
     public String toString() {
-        // TODO Auto-generated method stub
+        
 
         return totalBalance();
     }
